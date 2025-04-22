5317908f5bf1ef050c5a96cac4cee9f497a0885a
zol22 <zol2295@live.com>
2025-04-22 14:06:28 -0400

    add transition images

--- src/data/gallery.json.ts
+++ src/data/gallery.json.ts
@@ -4,20 +4,25 @@ import cd2 from "@assets/gallery/detailing/cd2.jpg"
 import cd4 from "@assets/gallery/detailing/cd4.jpg"
 import cd6 from "@assets/gallery/detailing/cd6.jpg"
 
+import cd5 from "@assets/gallery/detailing/cd5.png"
+import cd8 from "@assets/gallery/detailing/cd8.png"
+
+
+
 export const gallery = [
     {
       title: "Car Detailing",
       type: "image",
       image: cd2,
       alt: "Deep clean car exterior 1",
-      slug:"Deep-clean-car-exterior-1"
+      slug:"deep-clean-car-exterior-1"
     },
     {
       title: "Car Detailing",
       type: "image",
       image: cd4,
       alt: "Deep clean car exterior 2",
-      slug:"Deep-clean-car-exterior-2"
+      slug:"deep-clean-car-exterior-2"
 
 
     },
@@ -26,7 +31,23 @@ export const gallery = [
       type: "image",
       image: cd6,      
       alt: "Deep clean car interior 1",
-      slug:"Deep-clean-car-interior-1"
+      slug:"deep-clean-car-interior-1"
+
+    },
+    {
+      title: "Car Detailing",
+      type: "image",
+      image: cd5,      
+      alt: "Close-up of a freshly cleaned car tire and polished chrome rim in Orlando driveway",
+      slug:"white-suv-detailing"
+
+    },
+    {
+      title: "Car Detailing",
+      type: "image",
+      image: cd8,      
+      alt: "Shiny white SUV after car detailing, reflecting trees and sky in Orlando",
+      slug:"clean-tire-detail"
 
     }
   ];
